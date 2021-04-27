1878cc5144cc177c838dc8405748f065bfe80da2
Alexey Zhukov <alexey.zhukov@etu.u-bordeaux.fr>
2021-04-27 20:07:28 +0000

    Update StreeLE.java. CheckType() a ete ajoute

--- src/fr/ubordeaux/deptinfo/compilation/lea/stree/StreeLE.java
+++ src/fr/ubordeaux/deptinfo/compilation/lea/stree/StreeLE.java
@@ -2,11 +2,17 @@ package fr.ubordeaux.deptinfo.compilation.lea.stree;
 
 import fr.ubordeaux.deptinfo.compilation.lea.intermediate.BINOP;
 import fr.ubordeaux.deptinfo.compilation.lea.intermediate.Exp;
+import fr.ubordeaux.deptinfo.compilation.lea.type.Tag;
+import fr.ubordeaux.deptinfo.compilation.lea.type.Type;
 import fr.ubordeaux.deptinfo.compilation.lea.type.TypeException;
+import fr.ubordeaux.deptinfo.compilation.lea.type.TypeExpression;
+
+import static fr.ubordeaux.deptinfo.compilation.lea.type.Tag.BOOLEAN;
 
 public class StreeLE extends Stree {
 
 	private BINOP exp;
+	private Type type;
 
 	public StreeLE(Stree left, Stree right) throws StreeException, TypeException {
 		super(left, right);
@@ -16,6 +22,22 @@ public class StreeLE extends Stree {
 	@Override
 	public Exp getExp(){
 		return exp;
+	}
+
+	@Override
+	public Type getType() throws StreeException {
+		return type;
+	}
+
+	@Override
+	public boolean checkType() throws StreeException {
+		Type typeLeft = getLeft().getType();
+		Type typeRight = getRight().getType();
+		type = new TypeExpression(Tag.BOOLEAN);
+		if ((typeLeft != null) && (typeRight != null))
+			return typeLeft.assertEqual(typeRight);
+		else
+			throw new StreeException("Type error while checking null types !");
 	}
 
 }
