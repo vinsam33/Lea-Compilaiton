7994acef79dc1284a07a5150de55a498a7ff4590
Alexey Zhukov <alexey.zhukov@etu.u-bordeaux.fr>
2021-05-05 23:55:42 +0000

    Update StreeSWITCH.java

--- src/fr/ubordeaux/deptinfo/compilation/lea/stree/StreeSWITCH.java
+++ src/fr/ubordeaux/deptinfo/compilation/lea/stree/StreeSWITCH.java
@@ -1,7 +1,7 @@
 package fr.ubordeaux.deptinfo.compilation.lea.stree;
 
 import fr.ubordeaux.deptinfo.compilation.lea.intermediate.*;
-import fr.ubordeaux.deptinfo.compilation.lea.intermediate.temp.Label;
+import fr.ubordeaux.deptinfo.compilation.lea.type.Type;
 import fr.ubordeaux.deptinfo.compilation.lea.type.TypeException;
 
 public class StreeSWITCH extends Stree {
@@ -10,29 +10,38 @@ public class StreeSWITCH extends Stree {
 
 	public StreeSWITCH(Stree left, Stree right) throws TypeException, StreeException {
 		super(left, right);
+
+		System.out.println(right.getType());
 		this.stm = generateIntermediateCode();
 	}
 
 	@Override
 	public Stm generateIntermediateCode() throws StreeException {
-		Label label1 = new Label();
-		Label label2 = new Label();
-		Label label3 = new Label();
-		// label1:
-		// 	code du test
-		//  goto label2 si == 0 sinon goto label3
-		// label2:
-		//  corps de la boucle
-		//  goto label 1
-		// label3:
-		//  fin
-		return new SEQ(new LABEL(label1),
-				new SEQ(new CJUMP(CJUMP.Op.EQ, getLeft().getExp(), new CONST(0), label2, label3),
-						new SEQ(new LABEL(label2),
-								new SEQ(getRight().getStm(),
-										new SEQ(new JUMP(label1),
-												new LABEL(label3))))));
+		return getRight().getStm();
+	}
 
+	@Override
+	public boolean checkType() throws StreeException {
+		Type typeLeft = getLeft().getType();
+		String[] productTypes = getRight().getType().toString().split(" X ");
+		String typeLeftS = typeLeft + "";
+		boolean res = true;
+		int n = productTypes.length;
+
+		if (typeLeft != null) {
+			for(int i = 0; i < n; i++) {
+				if(!typeLeftS.equals(productTypes[i]) && !productTypes[i].equals("void"))
+					res = false;
+			}
+			return res;
+		}
+		else
+			throw new StreeException("Type error while checking null types !");
+	}
+
+	@Override
+	public Stm getStm() {
+		return stm;
 	}
 
 }
