cf6a21f5e69ee249426f60e6d0283127e5c360e0
Alexey Zhukov <alexey.zhukov@etu.u-bordeaux.fr>
2021-05-05 23:54:03 +0000

    Update StreeDEFAULT.java

--- src/fr/ubordeaux/deptinfo/compilation/lea/stree/StreeDEFAULT.java
+++ src/fr/ubordeaux/deptinfo/compilation/lea/stree/StreeDEFAULT.java
@@ -1,6 +1,10 @@
 package fr.ubordeaux.deptinfo.compilation.lea.stree;
 
+import fr.ubordeaux.deptinfo.compilation.lea.intermediate.JUMP;
+import fr.ubordeaux.deptinfo.compilation.lea.intermediate.LABEL;
+import fr.ubordeaux.deptinfo.compilation.lea.intermediate.SEQ;
 import fr.ubordeaux.deptinfo.compilation.lea.intermediate.Stm;
+import fr.ubordeaux.deptinfo.compilation.lea.intermediate.temp.Label;
 import fr.ubordeaux.deptinfo.compilation.lea.type.Tag;
 import fr.ubordeaux.deptinfo.compilation.lea.type.Type;
 import fr.ubordeaux.deptinfo.compilation.lea.type.TypeException;
@@ -9,11 +13,19 @@ import fr.ubordeaux.deptinfo.compilation.lea.type.TypeExpression;
 public class StreeDEFAULT extends Stree {
 
 	private Stm stm;
-	private Type type = new TypeExpression(Tag.VOID);
+	private Type type;
+	private Label labelFalse;
 
 	public StreeDEFAULT(Stree left) throws TypeException, StreeException {
 		super(left);
-		//this.stm = generateIntermediateCode();
+		this.type = new TypeExpression(Tag.VOID);
+		this.labelFalse = new Label();
+		this.stm = generateIntermediateCode();
+	}
+
+	@Override
+	public Stm generateIntermediateCode() throws StreeException {
+		return new SEQ(new LABEL(labelFalse), getLeft().getStm());
 	}
 
 	@Override
@@ -28,11 +40,13 @@ public class StreeDEFAULT extends Stree {
 
 	@Override
 	public boolean checkType() throws StreeException {
-		Type typeLeft = getLeft().getType();
-		if (typeLeft != null)
-			return typeLeft.assertEqual(new TypeExpression(Tag.VOID));
-		else
-			throw new StreeException("Type error while checking null types !");
+		return true;
 	}
+
+	@Override
+	public Label getLabelFalse() {
+		return labelFalse;
+	}
+
 
 }
