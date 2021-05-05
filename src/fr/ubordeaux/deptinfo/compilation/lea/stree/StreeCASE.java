4c085098d00512c9da56882fafbb6bbd080c51de
Alexey Zhukov <alexey.zhukov@etu.u-bordeaux.fr>
2021-05-05 23:54:53 +0000

    Update StreeCASE.java

--- src/fr/ubordeaux/deptinfo/compilation/lea/stree/StreeCASE.java
+++ src/fr/ubordeaux/deptinfo/compilation/lea/stree/StreeCASE.java
@@ -1,6 +1,7 @@
 package fr.ubordeaux.deptinfo.compilation.lea.stree;
 
-import fr.ubordeaux.deptinfo.compilation.lea.intermediate.Stm;
+import fr.ubordeaux.deptinfo.compilation.lea.intermediate.*;
+import fr.ubordeaux.deptinfo.compilation.lea.intermediate.temp.Label;
 import fr.ubordeaux.deptinfo.compilation.lea.type.Tag;
 import fr.ubordeaux.deptinfo.compilation.lea.type.Type;
 import fr.ubordeaux.deptinfo.compilation.lea.type.TypeException;
@@ -9,19 +10,26 @@ import fr.ubordeaux.deptinfo.compilation.lea.type.TypeExpression;
 public class StreeCASE extends Stree {
 
 	private Stm stm;
+	private Label labelFalse;
 	private Type type;
 
 	public StreeCASE(Stree left, Stree right) throws TypeException, StreeException {
 		super(left, right);
-		//System.out.println(right);
-		//this.stm = generateIntermediateCode();
+		this.type = new TypeExpression(Tag.NAME, left.getType()+"");
+		this.labelFalse = new Label();
+		this.stm = generateIntermediateCode();
+	}
+
+	@Override
+	public Stm generateIntermediateCode() throws StreeException {
+		return new SEQ(new LABEL(labelFalse), getRight().getStm());
 	}
 
 	@Override
 	public boolean checkType() throws StreeException {
 		Type typeLeft = getLeft().getType();
 		if (typeLeft != null)
-			return typeLeft.assertBoolean();
+			return true;
 		else
 			throw new StreeException("Type error while checking null types !");
 	}
@@ -35,5 +43,14 @@ public class StreeCASE extends Stree {
 	public Type getType() throws StreeException {
 		return type;
 	}
-	
+
+	@Override
+	public Label getLabelFalse() {
+		return labelFalse;
+	}
+
+	@Override
+	public Exp getExp() throws StreeException {
+		return getLeft().getExp();
+	}
 }
