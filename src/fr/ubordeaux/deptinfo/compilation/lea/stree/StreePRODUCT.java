c598ea84eb9c339b64963fe77d49bb431015d6bb
Alexey Zhukov <alexey.zhukov@etu.u-bordeaux.fr>
2021-05-06 20:00:12 +0000

    Update StreePRODUCT.java

--- src/fr/ubordeaux/deptinfo/compilation/lea/stree/StreePRODUCT.java
+++ src/fr/ubordeaux/deptinfo/compilation/lea/stree/StreePRODUCT.java
@@ -2,6 +2,7 @@ package fr.ubordeaux.deptinfo.compilation.lea.stree;
 
 import fr.ubordeaux.deptinfo.compilation.lea.intermediate.*;
 import fr.ubordeaux.deptinfo.compilation.lea.intermediate.temp.Label;
+import fr.ubordeaux.deptinfo.compilation.lea.intermediate.temp.LabelList;
 import fr.ubordeaux.deptinfo.compilation.lea.type.Tag;
 import fr.ubordeaux.deptinfo.compilation.lea.type.Type;
 import fr.ubordeaux.deptinfo.compilation.lea.type.TypeException;
@@ -11,26 +12,60 @@ public class StreePRODUCT extends Stree {
 
 	private Stm stm;
 	private Type type;
-	private Label labelTrue;
-	private Label labelFalse;
-
+	private Label labelFin;
+	private ExpList exps;
+	private LabelList labels;
 
 	public StreePRODUCT(Stree left, Stree right) throws TypeException, StreeException {
 		super(left, right);
 		this.type = new TypeExpression(Tag.PRODUCT, left.getType(), right.getType());
-		if(right.getType().assertEqual(new TypeExpression(Tag.VOID)))
-			this.labelFalse = right.getLabelFalse();
-		else
-			this.labelFalse = right.getLabelTrue();
-		this.labelTrue = new Label();
 		this.stm = generateIntermediateCode();
 	}
 
 	@Override
-	public Stm generateIntermediateCode() throws StreeException {
-		return new SEQ(new LABEL(labelTrue),
-						new SEQ (new CJUMP(CJUMP.Op.EQ, getLeft().getExp(), new CONST(0), getLeft().getLabelFalse(), labelFalse),
-							new SEQ(getLeft().getStm(), getRight().getStm())));
+	public Stm generateIntermediateCode() throws StreeException{
+		boolean vd = getRight().getType().assertEqual(new TypeExpression(Tag.VOID));
+		boolean cas;
+		if (getRight().getLabelTrue() == null || getRight().getLabelFalse() == null)
+			cas = false;
+		else
+			cas = true;
+			//cas = getRight().getLabelFalse().equals(getRight().getLabelTrue());
+
+		if(vd || cas) {
+			Label r = getRight().getLabelTrue();
+			Label l = getLeft().getLabelTrue();
+			if(cas && !vd) {
+				exps = new ExpList(getRight().getExp());
+				System.out.println(exps.getHead());
+				exps = new ExpList(getLeft().getExp(), exps);
+				System.out.println(exps.getHead());
+			}
+			if(vd) {
+				exps = new ExpList(getLeft().getExp());
+			}
+			labelFin = new Label();
+			labels = new LabelList(labelFin);
+			labels = new LabelList(r,labels); // default/\fin
+			labels = new LabelList(l,labels);
+
+			return new SEQ(new SEQ(getLeft().getStm(), new JUMP(labelFin)), new SEQ(getRight().getStm(), new LABEL(labelFin)));
+		}
+		else {
+			labels = getRight().getLabelList();
+			Label l = getLeft().getLabelTrue();
+			labels = new LabelList(l,labels);
+			exps = getRight().getExpList();
+			exps = new ExpList(getLeft().getExp(), exps);
+
+			LabelList finL = labels;
+			do{
+				finL = finL.getTail();
+			} while(finL.getTail() != null);
+			labelFin = finL.getHead();
+
+			return new SEQ(new SEQ(getLeft().getStm(), new JUMP(labelFin)), getRight().getStm());
+		}
 	}
 
 	@Override
@@ -48,13 +83,27 @@ public class StreePRODUCT extends Stree {
 	}
 
 	@Override
-	public Label getLabelFalse() {
-		return labelFalse;
+	public ExpList getExpList(){
+		return exps;
+	}
+
+	@Override
+	public LabelList getLabelList(){
+		return labels;
+	}
+
+	@Override
+	public Label getLabelFin(){
+		return labelFin;
 	}
 
 	@Override
 	public Label getLabelTrue() {
-		return labelTrue;
+		return null;
 	}
-	
+
+	@Override
+	public Label getLabelFalse() {
+		return null;
+	}
 }
