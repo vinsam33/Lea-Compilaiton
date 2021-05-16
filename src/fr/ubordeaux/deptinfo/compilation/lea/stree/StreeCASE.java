ecf8d91faa719c3a73642b93caee371ca03ec369
Alexey Zhukov <alexey.zhukov@etu.u-bordeaux.fr>
2021-05-16 00:57:15 +0000

    Update StreeCASE.java

--- src/fr/ubordeaux/deptinfo/compilation/lea/stree/StreeCASE.java
+++ src/fr/ubordeaux/deptinfo/compilation/lea/stree/StreeCASE.java
@@ -2,6 +2,7 @@ package fr.ubordeaux.deptinfo.compilation.lea.stree;
 
 import fr.ubordeaux.deptinfo.compilation.lea.intermediate.*;
 import fr.ubordeaux.deptinfo.compilation.lea.intermediate.temp.Label;
+import fr.ubordeaux.deptinfo.compilation.lea.intermediate.temp.Temp;
 import fr.ubordeaux.deptinfo.compilation.lea.type.Tag;
 import fr.ubordeaux.deptinfo.compilation.lea.type.Type;
 import fr.ubordeaux.deptinfo.compilation.lea.type.TypeException;
@@ -11,20 +12,25 @@ public class StreeCASE extends Stree {
 
 	private Stm stm;
 	private Type type;
-	private Label labelTrue;
+	private Label labelTrue; //case label
+	private Label labelFalse;//other case, default, fin
 	private Exp exp;
+	private Temp tmp;
 
 	public StreeCASE(Stree left, Stree right) throws TypeException, StreeException {
 		super(left, right);
 		this.type = new TypeExpression(Tag.NAME, left.getType()+"");
-		this.labelTrue = new Label();
 		this.stm = generateIntermediateCode();
-		this.exp = getLeft().getExp();
 	}
 
 	@Override
 	public Stm generateIntermediateCode() throws StreeException {
-		return new SEQ(new LABEL(labelTrue), getRight().getStm());
+		this.exp = getLeft().getExp();
+		this.labelTrue = new Label();
+		this.labelFalse = new Label();
+		this.tmp = new Temp();
+		TEMP tmp_stm = new TEMP(tmp);
+		return new SEQ(new CJUMP(CJUMP.Op.EQ, tmp_stm, exp, labelTrue, labelFalse), new SEQ(new LABEL(labelTrue),getRight().getStm()));
 	}
 
 	@Override
@@ -51,9 +57,18 @@ public class StreeCASE extends Stree {
 
 	@Override
 	public Label getLabelFalse() {
-		return labelTrue;
+		return labelFalse;
+	}
+
+	@Override
+	public Label getLabelFin(){
+		return null;
 	}
 
 	@Override
 	public Exp getExp() throws StreeException { return exp; }
+
+	@Override
+	public Temp getTemp() { return tmp;}
+
 }
