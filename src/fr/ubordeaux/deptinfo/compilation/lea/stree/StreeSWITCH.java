c43cbac896b836078fe0b8c3691e9131385ba042
Alexey Zhukov <alexey.zhukov@etu.u-bordeaux.fr>
2021-05-16 00:55:47 +0000

    Update StreeSWITCH.java

--- src/fr/ubordeaux/deptinfo/compilation/lea/stree/StreeSWITCH.java
+++ src/fr/ubordeaux/deptinfo/compilation/lea/stree/StreeSWITCH.java
@@ -2,37 +2,27 @@ package fr.ubordeaux.deptinfo.compilation.lea.stree;
 
 import fr.ubordeaux.deptinfo.compilation.lea.intermediate.*;
 import fr.ubordeaux.deptinfo.compilation.lea.intermediate.temp.Label;
-import fr.ubordeaux.deptinfo.compilation.lea.intermediate.temp.LabelList;
-import fr.ubordeaux.deptinfo.compilation.lea.type.Tag;
+import fr.ubordeaux.deptinfo.compilation.lea.intermediate.temp.TempList;
 import fr.ubordeaux.deptinfo.compilation.lea.type.Type;
 import fr.ubordeaux.deptinfo.compilation.lea.type.TypeException;
-import fr.ubordeaux.deptinfo.compilation.lea.type.TypeExpression;
 
 public class StreeSWITCH extends Stree {
 
 	private Stm stm;
-	private LabelList labels;
-	private ExpList exps;
+	private TempList temps;
 	private boolean product;
 	private boolean def;
 
 	public StreeSWITCH(Stree left, Stree right) throws TypeException, StreeException {
 		super(left, right);
-		Label labelTrue = right.getLabelTrue();
+		Label labelFin = right.getLabelFin();
 		product = true;
 		def = false;
-		if(labelTrue != null) {
+		if(labelFin == null) {
 			product = false;
-			labels = new LabelList(labelTrue);
-			Exp exp = right.getExp();
-			if(exp != null)
-				exps = new ExpList(exp);
-			else
+			Label labelTrue = right.getLabelTrue();
+			if(labelTrue == null)
 				def = true;
-		}
-		else {
-			labels = right.getLabelList();
-			exps = right.getExpList();
 		}
 
 		this.stm = generateIntermediateCode();
@@ -40,122 +30,23 @@ public class StreeSWITCH extends Stree {
 
 	@Override
 	public Stm generateIntermediateCode() throws StreeException {
-		//System.out.println(getRight().getLabelFin());
-		//System.out.println(getRight().getLabelList().getHead());
-		//System.out.println(getRight().getExpList().getHead());
 
 		if(product) {
-			LabelList ls = labels;
-			Label tF;
-			boolean def = true;
-
-
-			ExpList exs = exps;
-			Exp ex = exps.getHead();
-			Label tT = labels.getHead();
-
-			while(ls.getTail().hasNext()){
-				ls = ls.getTail();
-				exs = exs.getTail();
-				//System.out.println(ls.getHead());
-			}
-			if(exs.hasNext())
-				def = false;
-			
-			System.out.println(getLeft().getExp());
-			Stm tests;
-			tF = ls.getHead();
-			if(def) {
-				tests = new CJUMP(CJUMP.Op.EQ, getLeft().getExp(), ex, tT, tF);
-			}
-			else {
-				Label nl = new Label();
-
-				tests = new CJUMP(CJUMP.Op.EQ, getLeft().getExp(), ex, tT, nl);
-				Label ntF = ls.getTail().getHead();
-				tests = new SEQ(tests, new SEQ(new LABEL(nl), new CJUMP(CJUMP.Op.EQ, getLeft().getExp(), exs.getTail().getHead(), tF, ntF)));
-			}
-
-			exs = exps;
-			Label ntF;
-			ls = labels;
-
-			while(exs.hasNext()){
-				ls = ls.getTail();
-				tT = ls.getHead();
-				System.out.println("tT " +tT);
-				exs = exs.getTail();
-				ntF = tF;
-				if(!exs.hasNext()) {
-					Label tFF = ls.getHead();
-					if(tT.equals(tFF)){
-						System.out.println("tFF " +tFF);
-						tFF = ls.getHead();
-
-					}
-					tests = new SEQ(tests, new SEQ(new LABEL(ntF), new CJUMP(CJUMP.Op.EQ, getLeft().getExp(), ex, tT, tFF)));
-					//tests = getRight().getStm();
-				}
-				else {
-					ex = exs.getHead();
-					System.out.println("ex " +ex);
-					tF = new Label();
-					tests = new SEQ(tests, new SEQ(new LABEL(ntF), new CJUMP(CJUMP.Op.EQ, getLeft().getExp(), ex, tT, tF)));
-					//tests = getRight().getStm();
-				}
-			}
-
-			return new SEQ(tests, getRight().getStm());
-			//return getRight().getStm();
+			temps = getRight().getTempList();
+			TEMP tmp_stm = new TEMP(temps.getHead());
+			Stm tmp = new MOVE(tmp_stm,getLeft().getExp());
+			return new SEQ(tmp, getRight().getStm());
 		}
 		else {
-			Label tF = new Label();
 			if(def)
-				return new SEQ(getRight().getStm(), new LABEL(tF));
+				return getRight().getStm();
 			else {
-				Label tT = labels.getHead();
-				Exp ex = exps.getHead();
-				return new SEQ(new CJUMP(CJUMP.Op.EQ, getLeft().getExp(), ex, tT, tF), new SEQ(new SEQ(getRight().getStm(), new JUMP(tF)),new LABEL(tF)));
+				temps = new TempList(getRight().getTemp());
+				TEMP tmp_stm = new TEMP(temps.getHead());
+				Stm tmp = new MOVE(tmp_stm,getLeft().getExp());
+				return new SEQ(tmp, new SEQ(getRight().getStm(), new LABEL(getRight().getLabelFalse())));
 			}
 		}
-
-		/*LabelList ls = labels;
-		Label tT = labels.getHead();
-		Label tF = new Label();
-		LabelList defL = labels;
-		do{
-			defL = defL.getTail();
-		} while(defL.getTail().getTail() != null);
-		if(exps.getTail() == null)
-			tF = defL.getHead();
-		ExpList exs = exps;
-		Exp ex = exps.getHead();
-		if(tT.equals(tF)){
-			tF = defL.getTail().getHead();
-		}
-		Stm tests = new CJUMP(CJUMP.Op.EQ, getLeft().getExp(), ex, tT, tF);
-		Label ntF;
-
-		while(exs.getTail() != null){
-			ls = ls.getTail();
-			tT = ls.getHead();
-			exs = exs.getTail();
-			ex = exs.getHead();
-			ntF = tF;
-			if(exs.getTail() == null) {
-				Label tFF = defL.getHead();
-				if(tT.equals(tFF)){
-					tFF = defL.getTail().getHead();
-				}
-				tests = new SEQ(tests, new SEQ(new LABEL(ntF), new CJUMP(CJUMP.Op.EQ, getLeft().getExp(), ex, tT, tFF)));
-			}
-			else {
-				tF = new Label();
-				tests = new SEQ(tests, new SEQ(new LABEL(ntF), new CJUMP(CJUMP.Op.EQ, getLeft().getExp(), ex, tT, tF)));
-			}
-		}*/
-
-		//System.out.println(tests);
 	}
 
 	@Override
@@ -163,23 +54,20 @@ public class StreeSWITCH extends Stree {
 		Type typeLeft = getLeft().getType();
 		String[] productTypes = getRight().getType().toString().split(" X ");
 		String typeLeftS = typeLeft + "";
+		int n = productTypes.length;
 		boolean res = true;
 
 		if (typeLeft != null) {
-			if(product) {
-				int n = productTypes.length;
+			if(n > 1) {
 				for(int i = 0; i < n; i++) {
 					if(!typeLeftS.equals(productTypes[i]) && !productTypes[i].equals("void"))
 						res = false;
 				}
 			}
 			else {
-				if(!def) {
-					if(!typeLeftS.equals(productTypes[0])  && !productTypes[0].equals("void"))
-						res = false;
-				}
+				if(!typeLeftS.equals(productTypes[0]) && !productTypes[0].equals("void"))
+					res = false;
 			}
-
 			return res;
 		}
 		else
